e05b5e175f2b2c73f882e92bdaf90323d7d6d580
norman.argiolas <norman.argiolas@flosslab.com>
2019-10-28 18:22:13 +0100

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 let express = require('express');
 let http = require('http');
-let firebase = require('firebase');
+// let firebase = require('firebase');
 
 
 let app = express();
@@ -12,13 +12,13 @@ let server = http.createServer(app);
 
 require('./config/express')(app, express, routes, process.env.api_server_port, process.env.api_server_host, process.env.api_server_prefix);
 require('./config/www')(server);
-require('./config/firebase')(firebase);
+// require('./config/firebase')(firebase);
 
 
 /** Define here other controllers and injects the services**/
 require('./controllers/applicationController')(app);
 
-require('./controllers/api/userAPIController')(routes, firebase);
+// require('./controllers/api/userAPIController')(routes, firebase);
 require('./controllers/api/bookAPIController')(routes);
 
 /** Last controller Error Handler**/
